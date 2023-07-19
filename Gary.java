e17a8670f04092bcfc9012462ea4e377b9456914
adamada246 <78761055+adamada246@users.noreply.github.com>
2023-07-19 19:51:23 +0300

    Build 4
    Added cutting-edge new Gary setting and initializing overloaded methods using InputSource.
    Added toString method.

--- Gary.java
+++ Gary.java
@@ -1,18 +1,20 @@
+import java.io.InputStream;
 import java.util.Scanner;
-//Build 3
+//Build 4
 class Gary {
 	private Scanner gary;
 
 	Gary() {
 		gary = new Scanner(System.in);
 	}
-
 	Gary(Scanner g) {
 		gary = g;
 	}
-
+	Gary(InputStream source){
+		gary = new Scanner(source);
+	}
 	public int getBuild() {
-		return 3;
+		return 4;
 	}
 	public boolean isGary(String n) {
 		if (n.toLowerCase().contains("gary")) {
@@ -28,12 +30,17 @@ class Gary {
 	public Scanner birthGary(Scanner gary) {
 		return gary.reset();
 	}
-
+	public String toString() {
+		return "gary";
+	}
 	public Scanner getGary() {
 		return gary;
 	}
 
 	public void setGary(Scanner g) {
 		gary = g;
+	}
+	public void setGary(InputStream source) {
+		gary= new Scanner(source);
 	}
 }
